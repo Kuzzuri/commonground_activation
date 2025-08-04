75e4b6618fd2a37ba993afc57b4abd9f9bf53cfd
Kuzzuri <umuttengiz@gmail.com>
2025-08-04 22:36:53 +0300

    A/Redirect

--- script.js
+++ script.js
@@ -2,7 +2,36 @@ const params = new URLSearchParams(window.location.search)
 const uid = params.get('uid')
 const token = params.get('token')
 
-if(!uid | !token){
+
+function getMobilePlatform(){
+    const ua = navigator.userAgent
+
+    if(/android/i.test(ua)){
+        return "android"
+    }
+
+    if(/iPad|iPhone|iPod/.test(ua)){
+        return "ios"
+    }
+
+    return "other"
+}
+function redirectToApp(){
+    const platform = getMobilePlatform()
+
+    window.location.href = "commonground://"
+
+    setTimeout(() => {
+        if(platform === "android"){
+            window.location.href = "https://duofine.com"
+        }else if(platform === "ios"){
+            window.location.href = "https://duofine.com"
+        }
+    }, 1500)
+}
+
+
+if(!uid || !token){
     document.body.innerHTML = "<h1>Invalid activation link.</h1>"
 }else{
     async function sendActivationData(){
@@ -23,6 +52,7 @@ if(!uid | !token){
                   <h2>Redirecting to the app...</h2>
                   <button onclick="window.location.href='commonground://'">Open App</button>
                 `
+                redirectToApp()
             }else{
                 document.body.innerHTML = `<h1>Activation failed. Link may be invalid or expired.</h1>
                                             <h2>If the issue persists please contact support at cg@support.com
