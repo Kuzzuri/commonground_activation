cffe7cbc0f2dcd8dbeb85af11560d5ccea9a850c
Kuzzuri <umuttengiz@gmail.com>
2025-08-04 16:56:51 +0300

    A/Sending post

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ const token = params.get('token')
 if(!uid | !token){
     document.body.innerHTML = "<h1>Invalid activation link.</h1>"
 }else{
-    console.log(uid)
-    console.log(token)
+    async function sendActivationData(){
+        try{
+            let response = await fetch('https://commongroundapi-production.up.railway.app/user/activate/', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    uid: uid,
+                    token: token
+                })
+            })
+            if(response.ok){
+                document.body.innerHTML = `
+                  <h1>Account activated!</h1>
+                  <h2>Redirecting to the app...</h2>
+                  <button onclick="window.location.href='commonground://'">Open App</button>
+                `
+            }else{
+                document.body.innerHTML = `<h1>Activation failed. Link may be invalid or expired.</h1>
+                                            <h2>If the issue persists please contact support at cg@support.com
+                `
+            }
+            }catch(e){
+                document.body.innerHTML = "<h1>An error ocurred. Please try again</h1>"
+            }
+        }
+    sendActivationData()
 }
